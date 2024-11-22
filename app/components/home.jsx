706f9bac22832b223c7c7bdc6e64a20167e4ca2d
Muhammad Ali Usman <aliusmanijaz143@gmail.com>
2024-11-22 04:07:22 -0800

    new branch

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -6,6 +6,9 @@ const home = () => {
      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et aliquam deleniti quasi soluta! Modi rerum dolores nisi consectetur, a ducimus minima beatae itaque eaque repudiandae quasi, unde sapiente. Totam, a! </p>
      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et aliquam deleniti quasi soluta! Modi rerum dolores nisi consectetur, a ducimus minima beatae itaque eaque repudiandae quasi, unde sapiente. Totam, a! </p>
      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et aliquam deleniti quasi soluta! Modi rerum dolores nisi consectetur, a ducimus minima beatae itaque eaque repudiandae quasi, unde sapiente. Totam, a! </p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et aliquam deleniti quasi soluta! Modi rerum dolores nisi consectetur, a ducimus minima beatae itaque eaque repudiandae quasi, unde sapiente. Totam, a! </p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et aliquam deleniti quasi soluta! Modi rerum dolores nisi consectetur, a ducimus minima beatae itaque eaque repudiandae quasi, unde sapiente. Totam, a! </p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et aliquam deleniti quasi soluta! Modi rerum dolores nisi consectetur, a ducimus minima beatae itaque eaque repudiandae quasi, unde sapiente. Totam, a! </p>
      </div>
   )
 }
