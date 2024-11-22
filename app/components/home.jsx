d2237d986fa4cb30d47e97cda2447f4ab5d111ed
Muhammad Ali Usman <aliusmanijaz143@gmail.com>
2024-11-22 03:56:09 -0800

    Ali Ka code 2 code

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -3,21 +3,8 @@ import React from 'react'
 const home = () => {
   return (
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque officiis iste dignissimos dicta, beatae tempore hic eligendi ipsam at maiores consequatur temporibus aspernatur quia itaque illo. Voluptates laudantium eligendi cupiditate?</p>
-    </div>
+     <p>Zavyar L py char</p>
+     </div>
   )
 }
 
